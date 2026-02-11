9c7a3d9fc5ce84afc708a5b74827f5de3efc65e8
Nate <nasir.pk@gmail.com>
2026-02-11 17:08:29 +0500

    Add Rs. prefix to all currency amounts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat("en-PK", {
+  return "Rs. " + new Intl.NumberFormat("en-PK", {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(amount);
